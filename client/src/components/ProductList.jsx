5b2724403ea14fd17abb27ae5cb6a607fdb51010
Trong Doan <doantrong8777@gmail.com>
2022-09-17 01:04:52 -0700

    Product manager 2

---
+++ client/src/components/ProductList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+const ProductList = (props) => {
+    return (
+
+        <div>
+
+            <h1>All Product</h1>
+            {props.products.map((product, i) =>
+                <div>
+                    <Link to={`/${product._id}`} key={i}>{product.title}</Link>
+                </div>
+
+            )}
+        </div>
+    )
+
+
+
+}
+export default ProductList;
